c7f65a09a37196e2248f1ae8587d1659a30280f4
Neda Meily <nedarobatmeily@gmail.com>
2020-12-02 12:38:31 +1100

    Added haul bundler Added a form to example app with validation

--- RoktSampleApp/App.js
+++ RoktSampleApp/App.js
@@ -1,118 +1,271 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
+ * Demo application to showcase Rokt React Native SDK usage.
+ * 
+ * Copyright 2020 Rokt Pte Ltd Licensed under the Rokt Software Development Kit (SDK) Terms of Use Version 2.0 (the "License");
+ *  You may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at https://rokt.com/sdk-license-2-0/
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,findNodeHandle, TouchableOpacity, SafeAreaView, ScrollView} from 'react-native';
-import {Rokt, RoktEmbeddedView} from 'react-native-rokt-widget';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-const embeddedAndLightbox = "testiOS";
-const embeddedAndroid = "testAndroidLboxAndE";
-const roktViewName = "testTwoEmbedded"
+import React, { Component } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  findNodeHandle,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+import Toast from "react-native-toast-message";
+import { isNumeric, isEmpty, isNotEmpty, isValidJson } from "./utils/text-utils";
+import { DEFAULT_ATTRIBUTES, DEFAULT_TAG_ID, DEFAULT_VIEW_NAME, DEFAULT_COUNTRY } from "./utils/rokt-constants";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { Rokt, RoktEmbeddedView } from "react-native-rokt-widget";
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+
+    var attributes =  JSON.stringify(DEFAULT_ATTRIBUTES);
+
     this.placeholder1 = React.createRef();
     this.placeholder2 = React.createRef();
+    this.state = {
+      tagId: DEFAULT_TAG_ID,
+      viewName: DEFAULT_VIEW_NAME,
+      country: DEFAULT_COUNTRY,
+      targetElement1: "Location1",
+      targetElement2: "Location2",
+      attributes: attributes
+    };
   }
 
   onInitHandler = () => {
-    console.log(Rokt)
-    Rokt.initialize("2754655826098840951", "1.1");
-    console.log("Initialize");
-  }
+    if (isNotEmpty(this.state.tagId) && isNumeric(this.state.tagId)) {
+      Rokt.initialize(this.state.tagId, "1.1");
+      console.log("Initialize");
+    } else {
+      this.showToast("Tag ID must be a valid number");
+    }
+  };
+
+  showToast = (message) => {
+    Toast.show({
+      text1: "Invalid form",
+      text2: message,
+      type: "error",
+      position: "bottom",
+      autoHide: true,
+    });
+  };
 
   onExecuteHandler = () => {
-    attributes = {
-      "email": "j.smith@example.com",
-      "firstname": "Jenny",
-      "lastname": "Smith",
-      "mobile": "(323) 867-5309",
-      "postcode": "90210",
-      "country": "AU"
+    var stateCopy = JSON.parse(JSON.stringify(this.state));
+
+    if (!isValidJson(stateCopy.attributes)){
+      this.showToast("Attributes must be valid JSON");
+      return;
     }
-        
-    placeholders = {
-      "Location1": findNodeHandle(this.placeholder1.current),
-      "Location2": findNodeHandle(this.placeholder2.current),
+    
+    var attributes = JSON.parse(stateCopy.attributes);
+
+     attributes["country"] = this.state.country
+
+    var placeholders = {};
+    placeholders[this.state.targetElement1] = findNodeHandle(
+      this.placeholder1.current
+    );
+    placeholders[this.state.targetElement2] = findNodeHandle(
+      this.placeholder2.current
+    );
+
+    if (isEmpty(this.state.viewName)) {
+      this.showToast("View Name cannot be empty");
     }
 
-    Rokt.execute(roktViewName, attributes, placeholders,
-    x => {
-      console.log("Placement OnLoad Callback");
+    Rokt.execute(this.state.viewName, attributes, placeholders, (x) => {
+      console.log("Widget OnLoad Callback");
     });
     console.log("Execute");
-  }
+  };
 
   render() {
-    return (              
-      
-	<SafeAreaView style={styles.containerScroll}>
-		<ScrollView style={styles.scrollView}>
-			<View style={styles.container}>
-				<Text style={styles.welcome}>Welcome to React Native</Text>
-				<Text style={styles.roktMessage}>Rokt Widget Integration!</Text>
-				<Text style={styles.instructions}>{instructions}</Text>
-				<TouchableOpacity onPress={this.onInitHandler}>
-					<Text style={styles.button}>Initialize</Text>
-				</TouchableOpacity>
-				<TouchableOpacity onPress={this.onExecuteHandler}>
-					<Text style={styles.button}>Execute</Text>
-				</TouchableOpacity>
-			</View>
-      {/* <RoktWidget ref={this.widgetRef1} palcementName={"Location1"} ></RoktWidget> */}
-			<RoktEmbeddedView ref={this.placeholder1} placeholderName={"Location1"} ></RoktEmbeddedView>
-			<RoktEmbeddedView ref={this.placeholder2} placeholderName={"Location2"} ></RoktEmbeddedView>
-			<View style={styles.afterWidget}/>
-		</ScrollView>
-	</SafeAreaView>);
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <View style={styles.body}>
+
+              <View style={styles.sectionContainer}>
+                <Text style={[styles.sectionTitle, { marginBottom: 20 }]}>
+                  Welcome to Rokt Demo{" "}
+                </Text>
+                <Text style={[styles.label, { color: Colors.black }]}>
+                  Rokt Tag ID:
+                </Text>
+                <TextInput
+                  style={styles.textInput}
+                  value={this.state.tagId}
+                  onChangeText={(tagId) => this.setState({ tagId })}
+                />
+
+                <View style={styles.horizontalContainer}>
+                  <View style={[{ height: 50, flex: 1, marginRight: 5 }]}>
+                    <Text style={[styles.label, { color: Colors.black }]}>
+                      View Name:
+                    </Text>
+                    <TextInput
+                      style={styles.textInput}
+                      value={this.state.viewName}
+                      onChangeText={(viewName) => this.setState({ viewName })}
+                    />
+                  </View>
+                  <View>
+                    <Text style={[styles.label, { color: Colors.black }]}>
+                      Country
+                    </Text>
+                    <TextInput
+                      style={styles.textInput}
+                      value={this.state.country}
+                      onChangeText={(country) => this.setState({ country })}
+                    />
+                  </View>
+                </View>
+
+                <Text style={[styles.label, { color: Colors.black }]}>
+                  Target Element
+                </Text>
+                <TextInput
+                  style={styles.textInput}
+                  value={this.state.targetElement1}
+                  onChangeText={(targetElement1) =>
+                    this.setState({ targetElement1 })
+                  }
+                />
+                <Text style={[styles.label, { color: Colors.black }]}>
+                  Target Element
+                </Text>
+                <TextInput
+                  style={styles.textInput}
+                  value={this.state.targetElement2}
+                  onChangeText={(targetElement2) =>
+                    this.setState({ targetElement2 })
+                  }
+                />
+                <Text style={[styles.label, { color: Colors.black }]}>
+                  Attributes
+                </Text>
+                <TextInput
+                  multiline
+                  numberOfLines={4}
+                  style={styles.multiLineText}
+                  value={this.state.attributes}
+                  onChangeText={(attributes) => {
+                    console.log(attributes);
+                    this.setState({ attributes: attributes })
+                  }}
+                />
+
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity
+                    onPress={this.onInitHandler}
+                    style={[{ height: 50, flex: 1 }]}>
+                    <Text style={styles.button}>Initialize</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={this.onExecuteHandler}
+                    style={[{ height: 50, flex: 1 }]}>
+                    <Text style={styles.button}>Execute</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+
+            <RoktEmbeddedView
+              ref={this.placeholder1}
+              placeholderName={this.state.targetElement1}
+            ></RoktEmbeddedView>
+            <RoktEmbeddedView
+              ref={this.placeholder2}
+              placeholderName={this.state.targetElement2}
+            ></RoktEmbeddedView>
+          </ScrollView>
+          <Toast ref={(ref) => Toast.setRef(ref)} />
+        </SafeAreaView>
+      </>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: "absolute",
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "600",
+    color: Colors.black,
   },
-  roktMessage: {
-    fontSize: 20,
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  label: {
+    fontWeight: "700",
+  },
+  multiLineText: {
+    maxHeight: 80,
+    borderColor: "gray",
+    borderWidth: 1,
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: "600",
+    padding: 4,
+    paddingRight: 12,
+    textAlign: "right",
+  },
+  textInput: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
   },
   button: {
-    backgroundColor: '#4ba9c8',
-    borderColor: 'white',
+    flex: 1,
+    backgroundColor: "#4ba9c8",
+    borderColor: "white",
     borderWidth: 1,
     borderRadius: 12,
-    color: 'white',
-    fontSize: 24,
-    fontWeight: 'bold',
-    overflow: 'hidden',
-    padding: 12,
-    textAlign:'center',
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    padding: 9,
+    textAlign: "center",
   },
-  afterWidget: {
-    height: 10,
-    backgroundColor: '#121212'
-  }
+  buttonContainer: {
+    marginTop: 10,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "stretch",
+  },
+  horizontalContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "stretch",
+  },
 });
