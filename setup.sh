c8b7cf34c55c071dc8a04d5c5f91cc3302b8e1dd
Neda Meily <nedarobatmeily@gmail.com>
2021-02-10 14:15:03 +1100

    Testing publishing with pipeline

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ./RoktSampleApp
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs 
+npm install
+cd ./android
+bundle install
