9074b23b621023638187033fe06f19e086eedbb5
Sahil Suri <sahil.suri@rokt.com>
2022-06-30 11:35:47 +1000

    RN test commit

--- RoktSampleApp/android/app/src/androidTest/java/com/roktsampleapp/TestUtils.java
+++ RoktSampleApp/android/app/src/androidTest/java/com/roktsampleapp/TestUtils.java
@@ -2,6 +2,7 @@ package com.roktsampleapp;
 
 import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.BoundedMatcher;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -10,6 +11,12 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static java.lang.Thread.sleep;
 import static org.hamcrest.Matchers.containsString;
+
+import android.view.View;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 
 
 public class TestUtils {
@@ -51,5 +58,41 @@ public class TestUtils {
 
     public static Boolean hasMaximumTimeExceeded(Long startTime) {
         return System.currentTimeMillis() - startTime > WAIT_TIME_TOTAL;
+    }
+
+    public static Matcher<View> withIndex(final Matcher<View> matcher, final int index) {
+        return new TypeSafeMatcher<View>() {
+            int currentIndex = 0;
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with index: ");
+                description.appendValue(index);
+                matcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                return matcher.matches(view) && currentIndex++ == index;
+            }
+        };
+    }
+
+    public static Matcher<View> withMinHeight(final float expectedHeight) {
+        return new BoundedMatcher<View, View>(View.class) {
+
+            @Override
+            public boolean matchesSafely(View target) {
+                float pixels = target.getHeight();
+                float actualHeight = pixels / target.getResources().getDisplayMetrics().scaledDensity;
+                return Float.compare(actualHeight, expectedHeight) >= 0;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with Height: ");
+                description.appendValue(expectedHeight);
+            }
+        };
     }
 }
