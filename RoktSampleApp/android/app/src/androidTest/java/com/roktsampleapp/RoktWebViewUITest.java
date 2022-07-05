8ec1bf68024aa71f1f6251dd9c3e9f9b4486ff20
Sahil Suri <sahil.suri@rokt.com>
2022-07-05 11:40:14 +1000

    RN test commit (#57)

--- RoktSampleApp/android/app/src/androidTest/java/com/roktsampleapp/RoktWebViewUITest.java
+++ RoktSampleApp/android/app/src/androidTest/java/com/roktsampleapp/RoktWebViewUITest.java
@@ -10,23 +10,23 @@
 package com.roktsampleapp;
 
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static java.lang.Thread.sleep;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static java.lang.Thread.sleep;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anyOf;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class RoktWebViewUITest {
@@ -37,25 +37,21 @@ public class RoktWebViewUITest {
     @Test
     public void checkPlacementsLoadCorrectly() throws Throwable {
         TestUtils.waitUntilDisplayed("Welcome to Rokt Demo ");
-        onView(anyOf(withText("Welcome to Rokt Demo "))).check(matches(isDisplayed()));
+        sleep(2000);
+        onView(withText("Welcome to Rokt Demo ")).check(matches(isDisplayed()));
 
         // Setup
-        onView(allOf(withContentDescription("input_tag_id"), isDisplayed())).perform(replaceText("2731619347947643042"));
-        onView(allOf(withContentDescription("input_view_name"), isDisplayed())).perform(replaceText("androidMultipleQA"));
-        onView(allOf(withContentDescription("input_stage_env"), isDisplayed())).perform(click());
+        onView(allOf(withContentDescription("input_tag_id"), isDisplayed())).perform(replaceText("2754655826098840951"));
+        onView(allOf(withContentDescription("input_view_name"), isDisplayed())).perform(replaceText("testTwoEmbedded"));
 
-        onView(anyOf(withText("Initialize"))).perform(click());
-        sleep(1000);
-        onView(anyOf(withText("Execute"))).perform(click());
+        onView(withText("Initialize")).perform(scrollTo(), click());
+        sleep(2000);
+        onView(withText("Execute")).perform(scrollTo(), click());
         sleep(6000);
 
-        // Check for lightbox placement
-        TestUtils.waitUntilPartiallyDisplayed("This is a test widget");
-        onView(allOf(withText("No thanks"), isDisplayed())).perform(click());
-        onView(allOf(withText("No thanks"), isDisplayed())).perform(click());
-
-        //Check for Embedded placement
-        TestUtils.waitUntilPartiallyDisplayed("This is a test widget for automated");
+        //onView(TestUtils.withIndex(withId(R.id.parentLayout), 0)).perform(scrollTo());
+        onView(TestUtils.withIndex(withId(R.id.parentLayout), 0)).check(matches(TestUtils.withMinHeight(100)));
+        onView(TestUtils.withIndex(withId(R.id.parentLayout), 1)).check(matches(TestUtils.withMinHeight(100)));
     }
 }
 
