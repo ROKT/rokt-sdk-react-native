99885d9aac70e8e206e05f2a5ab53b50fca58a4d
Sahil Suri <sahil.suri@rokt.com>
2022-09-02 15:38:00 +1000

    uncomment test

--- RoktSampleApp/ios/RoktSampleAppUITests/RoktSampleAppUITests.swift
+++ RoktSampleApp/ios/RoktSampleAppUITests/RoktSampleAppUITests.swift
@@ -40,7 +40,6 @@ class RoktSampleAppUITests: XCTestCase {
     XCTAssert(app.buttons["Rokt Privacy Policy"].exists, "Rokt Privacy Policy exists" )
   }
 
-/*
   func testOverlay() throws {
     // UI tests must launch the application that they test.
     let app = XCUIApplication()
@@ -61,7 +60,6 @@ class RoktSampleAppUITests: XCTestCase {
     XCTAssert(app.staticTexts["Test Title"].exists, "test title exist" )
     XCTAssert(app.buttons["Example Privacy Policy"].exists, "Example Privacy Policy exists" )
   }
-*/
 
   func waiting(_ secounds: TimeInterval) {
     _ = XCTWaiter.wait(for: [expectation(description: "Wait for n seconds")], timeout: secounds)
