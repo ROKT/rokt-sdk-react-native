7eb8ffaaa3adcff7b56ef9a2d72607f65c86d052
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-08-10 16:34:20 +1000

    Fix script

--- infra/create_tag.sh
+++ infra/create_tag.sh
@@ -4,7 +4,7 @@ set -eu
 
 # $1 version
 
-WIDGET_DIR="${dirname $0}/../Rokt.Widget"
+WIDGET_DIR="$(dirname $0)/../Rokt.Widget"
 
 cd $WIDGET_DIR
 
@@ -14,5 +14,5 @@ git config user.name "Buildkite"
 git add ./android/build.gradle
 git add rokt-react-native-sdk.podspec
 git diff-index --quiet HEAD || git commit -m "v$1"
-git tag -a v$1 -m "Automated release v$1" || echo "Tag already exists"
-git push origin v$1
+git tag -a "v$1" -m "Automated release v$1" || echo "Tag already exists"
+git push origin "v$1"
