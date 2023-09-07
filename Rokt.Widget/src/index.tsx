503cafc83a619d2334112affeb55a14e6a4a9ca0
Lewis Krishnamurti <122851605+lewis-rokt@users.noreply.github.com>
2023-09-08 09:46:25 +1000

    [NO-JIRA] Change to record (#106)

--- Rokt.Widget/src/index.tsx
+++ Rokt.Widget/src/index.tsx
@@ -4,8 +4,8 @@ import 'react-native'
 
 export interface RNRoktWidget {
     initialize(roktTagId: string, appVersion: string): void;
-    execute(viewName: string, attributes: Map<string, string>, placeholders: Map<string, number>): void;
-    execute2Step(viewName: string, attributes: Map<string, string>, placeholders: Map<string, number>): void;
+    execute(viewName: string, attributes: Record<string, string>, placeholders: Record<string, number>): void;
+    execute2Step(viewName: string, attributes: Record<string, string>, placeholders: Record<string, number>): void;
     setFulfillmentAttributes(attributes: Map<string, string>): void;
     setEnvironmentToStage(): void;
     setEnvironmentToProd(): void;
