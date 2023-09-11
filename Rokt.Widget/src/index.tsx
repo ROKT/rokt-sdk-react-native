fd2f546f9bf0ab8e4bee2091912d02ad48c12ab7
Lewis Krishnamurti <122851605+lewis-rokt@users.noreply.github.com>
2023-09-11 13:09:36 +1000

    [MP-2722] Update types (#108)

--- Rokt.Widget/src/index.tsx
+++ Rokt.Widget/src/index.tsx
@@ -4,8 +4,14 @@ import 'react-native'
 
 export interface RNRoktWidget {
     initialize(roktTagId: string, appVersion: string): void;
-    execute(viewName: string, attributes: Map<string, string> | Record<string, string>, placeholders: Map<string, number> | Record<string, number>, callback: () => void): void;
-    execute2Step(viewName: string, attributes: Map<string, string> | Record<string, string>, placeholders: Map<string, number> | Record<string, number>, callback: () => void): void;
+    execute(viewName: string, attributes: Record<string, string>, placeholders: Record<string, number | null>, callback: () => void): void;
+    /** @deprecated execute with type {@link Map} is depreacted, use signature with {@link Record} instead*/
+    execute(viewName: string, attributes: Map<string, string>, placeholders: Map<string, number>, callback: () => void): void;
+    execute2Step(viewName: string, attributes: Record<string, string>, placeholders: Record<string, number | null>, callback: () => void): void;
+    /** @deprecated execute2Step with type {@link Map} is depreacted, use signature with {@link Record} instead*/
+    execute2Step(viewName: string, attributes: Map<string, string>, placeholders: Map<string, number>, callback: () => void): void;
+    setFulfillmentAttributes(attributes: Record<string, string>): void;
+    /** @deprecated setFulfillmentAttributes with type {@link Map} is depreacted, use signature with {@link Record} instead*/
     setFulfillmentAttributes(attributes: Map<string, string>): void;
     setEnvironmentToStage(): void;
     setEnvironmentToProd(): void;
