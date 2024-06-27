72890697824f2cd159ef13c86fe212e3d7bc3918
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2024-06-27 10:11:44 +1000

    Re-export

--- Rokt.Widget/src/index.tsx
+++ Rokt.Widget/src/index.tsx
@@ -1,8 +1,9 @@
 import 'react-native'
 import { NativeModules } from 'react-native';
 import { RoktEmbeddedView } from './rokt-embedded-view'
-import { Rokt } from './Rokt'
+import { Rokt, IRoktConfig, RoktConfigBuilder, ColorMode } from './Rokt'
 
 const { RoktEventManager } = NativeModules;
 
-export {RoktEmbeddedView, Rokt, RoktEventManager};
+export {RoktEmbeddedView, Rokt, RoktEventManager, RoktConfigBuilder, ColorMode};
+export type {IRoktConfig};
