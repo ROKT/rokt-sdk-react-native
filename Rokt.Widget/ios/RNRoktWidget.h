290f0b8e3dff3ad17599f791fe8353a6a731ae17
Danial Motahari <49017604+danial-rokt@users.noreply.github.com>
2024-08-01 15:07:49 +1000

    refactor RCTBridgeModule imports (#142)

--- Rokt.Widget/ios/RNRoktWidget.h
+++ Rokt.Widget/ios/RNRoktWidget.h
@@ -8,11 +8,7 @@
 //  You may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at https://rokt.com/sdk-license-2-0/
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
 #import <Rokt_Widget/Rokt_Widget-Swift.h>
 #import "RoktEventManager.h"
 
